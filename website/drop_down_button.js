ce19c0d2069a0510d9604deebe72e9f0bd0b6b22
pysor2 <erikssonrazze@gmail.com>
2025-09-24 14:37:32 +0200

    Changed language selection
    Made language selection into a drop down menu to facilitate future language options

--- website/drop_down_button.js
+++ website/drop_down_button.js
@@ -2,3 +2,19 @@ function onDropDownButtonClick() {
     const MENU = document.querySelector(".dropDownMenu");
     MENU.classList.toggle("show");
 }
+function flagDropDown() {
+    const MENU = document.querySelector(".flagDropDownMenu");
+    MENU.classList.toggle("show");
+
+    window.onclick = function (event) {
+        if (!event.target.matches('.flagDropDown')) {
+            var dropdowns = document.getElementsByClassName("flagDropDownMenu");
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
