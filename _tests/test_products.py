ff212ad631abbf0b7cda1696b4ad5fc4de50b9fa
pysor2 <erikssonrazze@gmail.com>
2025-09-29 10:41:57 +0200

    Added deal of the day
    Added a discount that rotates depending on day of the week as well as tests for that discount

--- _tests/test_products.py
+++ _tests/test_products.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
+import datetime
+
 
 # Settings for how testing runs
 dontCloseBrowser = False  # if true then the browser is kept open after the tests are finished, otherwise it is closed
@@ -67,6 +69,50 @@ class testVAT(TestCase):
         self.browser.refresh()
         self.browser.find_element(By.CSS_SELECTOR, ".priceTag")  # wait for prices to load
         self.assertIn("320", self.browser.page_source)
+
+
+class testCut(TestCase):
+    # setUpClass is run BEFORE the FIRST test
+    @classmethod
+    def setUpClass(cls):
+        chr_options = Options()
+
+        if dontCloseBrowser:
+            chr_options.add_experimental_option("detach", True)
+
+        if hideWindow:
+            chr_options.add_argument("--headless")
+
+        # Wait for elements to appear at most 2 seconds before throwing an exception
+        chr_options.timeouts = {"implicit": 2000}
+
+        cls.browser = webdriver.Chrome(options=chr_options)
+
+    # tearDownClass is run AFTER the LAST test
+    @classmethod
+    def tearDownClass(cls):
+        pass  # Do nothing
+
+    # setUp is run BEFORE EVERY TEST
+    def setUp(self):
+        self.browser.delete_all_cookies()  # clear cookies between test
+        self.browser.get("about:blank")  # go to blank page to avoid influence from prior tests
+
+    # tearDown is run AFTER EACH TEST
+    def tearDown(self):
+        pass  # Do nothing
+
+    # HERE THE TESTS BEGINS
+    # Discounted price testing (products.html)
+    def testDiscounted(self):
+        self.browser.get("http://localhost:8000/website/products.html")
+        self.browser.execute_script("createProducts(new Date('2025-09-28T12:00:00'))")  # sets day to a Sunday
+        devPoduct = self.browser.find_element(By.XPATH, "//*[div='DEVPRODUCT']/div[span=1111]/..")  # Finds the dev product and waits for it to have the price 1111 within a span tag
+        discountedPrice = devPoduct.find_element(By.CSS_SELECTOR, ".discounted")
+        styles = discountedPrice.value_of_css_property("text-decoration")
+        styles = styles.split(" ", 1)
+        self.assertIn("line-through", styles)
+        self.assertIn("rgb(128, 128, 128)", styles)
 
 
 # this bit is here so that the tests run if the file is run as a regular python program
--- website/products.js
+++ website/products.js
@@ -1,4 +1,4 @@
-async function createProducts() {
+async function createProducts(date) {
     const ARRAY = await fetchProducts()
 
     for (let i = 0; i < ARRAY.length; i++) {
@@ -8,6 +8,9 @@ async function createProducts() {
         const PRODUCT_NAME = document.createElement('div')
         const PRODUCT_PRICE_TAG = document.createElement('div')
         const PRODUCT_PRICE = document.createElement('span')
+        const CUT_PRICE_TAG = document.createElement('div')
+        const CUT_PRICE = document.createElement('span')
+
 
         // If the product is a min price product, add "From:" before the price
         if (ARRAY[i].is_min_price) {
@@ -21,6 +24,7 @@ async function createProducts() {
         PRODUCT_DIV.className = 'product'
         PRODUCT_NAME.className = 'productDescription'
         PRODUCT_PRICE_TAG.className = 'priceTag'
+        CUT_PRICE_TAG.className = 'priceTag'
 
         // Set product name and currency unit 
         PRODUCT_NAME.textContent = ARRAY[i]["product_name"]
@@ -30,15 +34,31 @@ async function createProducts() {
         PRODUCT_PRICE.textContent = ARRAY[i].product_price
         PRODUCT_IMAGE.src = ARRAY[i].product_image
 
+        // Checks if flower is discounted today and shows discounted price
+        let Day = date.getDay()
+        const DISCOUNTS = ARRAY[i]["day_of_discount"]
+        if (DISCOUNTS != null) {
+            DISCOUNTS.forEach(element => {
+                if (element == Day) {
+                    PRODUCT_PRICE_TAG.classList.add('discounted')
+                    CUT_PRICE.textContent = ARRAY[i].discount_price;
+                    CUT_PRICE_TAG.appendChild(CUT_PRICE);
+                    let unit_discount = document.createTextNode("kr");
+                    CUT_PRICE_TAG.appendChild(unit_discount);
+                }
+            });
+        }
+
         // Appends elements to the DOM
         PRODUCT_DIV.appendChild(PRODUCT_IMAGE)
         PRODUCT_DIV.appendChild(PRODUCT_NAME)
         PRODUCT_PRICE_TAG.appendChild(PRODUCT_PRICE)
         PRODUCT_PRICE_TAG.appendChild(unit)
         PRODUCT_DIV.appendChild(PRODUCT_PRICE_TAG)
+        PRODUCT_DIV.appendChild(CUT_PRICE_TAG)
         document.getElementsByClassName('productsPage')[0].appendChild(PRODUCT_DIV)
     }
     toggleVAT()
 }
 
-createProducts()
+createProducts(new Date)
