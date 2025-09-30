a7efc054bb1ebcbb478dac9ccc7cc1ebd724fbaf
Gustav Holmberg <gustav.lastname@gmail.com>
2025-09-30 13:42:22 +0200

    fix to respond on delikat.ntig.dev/admin

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
+app.get('/admin', (req, res) => {
     console.log(req)
     res.send('Hello World from express!' + req)
 })
